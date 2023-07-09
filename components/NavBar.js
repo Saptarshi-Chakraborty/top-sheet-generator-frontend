4e2c9a7af85df6ba9d01922ad738a91cfdd4dd8e
Saptarshi Chakraborty <saptarshichakraborty04@outlook.com>
2023-07-09 19:55:37 +0530

    Completed the Navbar component

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,17 +1,50 @@
 'use client';
 
+import Link from 'next/link';
 import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-dark bg-dark">
-            <div className="container-fluid justify-content-center">
-                <span className="navbar-brand">
-                    <img src="./favicon.png" alt="FIEM" width="30" height="24" className="d-inline-block align-text-top" />
-                    <b> GENERATE&nbsp; TOP&nbsp; SHEET</b>
-                </span>
-            </div>
-        </nav>
+        <>
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+                <div className="container-fluid">
+
+                    <span className="navbar-brand">
+                        <img src="./favicon.png" alt="FIEM" width="30" height="24" className="d-inline-block align-text-top" />
+                        <b> TOP&nbsp; SHEET&nbsp; GENERATOR</b>
+                    </span>
+
+                    {/* Hamburger for Small Screen Navigation */}
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    {/* Options */}
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0" style={{ visibility: "hidden" }}>
+                        </ul>
+
+                        <div className="me-5">
+                            <ul className="d-flex navbar-nav me-auto mb-2 mb-lg-0 gap-2">
+
+                                <li className="nav-item">
+                                    <Link className="nav-link active" aria-current="page" href="/">Home</Link>
+                                </li>
+
+                                <li className="nav-item">
+                                    <Link className="nav-link active" aria-current="page" href="/allpdfs">All PDFs</Link>
+                                </li>
+
+                                <li className="nav-item">
+                                    <Link className="nav-link active" aria-current="page" href="/feedback">Feedback</Link>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
     )
 }
 
--- components/home/Body.js
+++ components/home/Body.js
@@ -10,8 +10,7 @@ const Body = () => {
 
     return (
         <div className="container my-3">
-            <h3 className="text-decoration-underline mb-0">Enter below details to generate your top sheet for CA2 exam (1st Year)</h3>
-            <small className="text-muted text-decoration-none mb-2">*(No information will be stored on the server)</small>
+            <h3 className="text-decoration-underline mb-3 main-heading">Enter below details to generate your top sheet for CA2 exam (1st Year)</h3>
 
             <SubmitForm isLoading={isLoading} setIsLoading={setIsLoading} />
             <LinkToAllPdfs isLoading={isLoading} />
